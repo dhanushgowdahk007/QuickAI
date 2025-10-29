41390eabd68b1fc16f2b02327df11c156264c14e
Dhanush Gowda H K <dhanushgowdahk04@gmail.com>
2025-10-30 02:21:36 +0530

    Design Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Navbar from '../components/navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <AiTools />
+            <Testimonial />
+        </>
+    )
 }
 
 export default Home
