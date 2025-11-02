0f1cb19165fb2eb7ee49abef5bf6c5d882b1e1d0
Dhanush Gowda H K <dhanushgowdahk04@gmail.com>
2025-11-02 22:11:01 +0530

    implement navigation and protect functionality in sidebar component

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -3,16 +3,18 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets';
 import { Menu, X } from 'lucide-react';
 import Sidebar from '../components/Sidebar';
+import { SignIn, useUser } from '@clerk/clerk-react';
 
 const Layout = () => {
 
     const navigate = useNavigate();
     const [sidebar, setSidebar] = useState(false);
+    const { user } = useUser();
 
-    return (
+    return user ? (
         <div className='flex flex-col items-start justify-start h-screen'>
             <nav className='w-full px-8 min-h-14 items-center justify-between border-b border-gray-200'>
-                <img src={assets.logo} alt="" onClick={ () => navigate('/') } />
+                <img className='cursor-pointer w-32 sm:w-44' src={assets.logo} alt="" onClick={ () => navigate('/') } />
                 {
                     sidebar ? <X onClick={ () => setSidebar(false) } className='w-6 h-6 text-gray-600 sm:hidden'/> 
                     : <Menu onClick={ () => setSidebar(true) } className='w-6 h-6 text-gray-600 sm:hidden'/>
@@ -24,6 +26,10 @@ const Layout = () => {
                     <Outlet />
                 </div>
             </div>
+        </div>
+    ) : (
+        <div className='flex items-center justify-center h-screen'>
+            <SignIn />
         </div>
     )
 }
